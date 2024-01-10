e8173c0d840fdd2a30cd67eb42728ec62e8c0e10
Alex <alexvnep@gmail.com>
2024-01-10 13:44:20 +0200

    'Add changeSize function to change grid size based on prompt from user
    Prompt is executed on click of gridButton eventListener. The input is then passed into a number from a string. As long as if statement is true then defaultSize becomes makeGrid(newSize).'

--- scripts.js
+++ scripts.js
@@ -1,5 +1,20 @@
 const container = document.querySelector('.container');
+const gridButton = document.querySelector('button');
 //const cell = document.querySelectorAll('.row');
+
+let defaultSize = makeGrid(16);
+
+function changeSize () {
+  let newSize = prompt('What size grid would you like to choose?');
+  let desiredValue = parseInt(newSize);
+  if (desiredValue >= 4 && desiredValue <= 100) {
+    defaultSize =makeGrid(newSize);
+  } else {
+    alert("Enter a digit from 1-100 range!");
+  }
+}
+
+gridButton.addEventListener('click', changeSize);
 
 function makeGrid (size) {  
   for (let i = 0; i < size; i++) {
@@ -19,5 +34,5 @@ function hover (e) {
   e.target.style.backgroundColor = 'red';
   }
 
-makeGrid(16);
+
 
